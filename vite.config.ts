cb1e55992490129da7a29f1d9623d8e508649e89
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-24 20:25:21 +0630

    Building a NavBar.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 9000,
+      open: true,
+    },
+  };
+});
