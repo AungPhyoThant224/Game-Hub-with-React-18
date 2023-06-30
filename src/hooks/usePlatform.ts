902a4ef68da7af6645d5c21844583891e20bb6fe
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-06-30 10:37:04 +0630

    Exercise: Fetching Platform.

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,10 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import platform from "../data/platform";
+import { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatform = () => ({ data: platform, isLoading: false, error: null });
+const usePlatform = () => {
+  return useQuery<FetchResponse<Platform>, Error>({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platform.length, results: platform },
+  });
+};
 
 export default usePlatform;
