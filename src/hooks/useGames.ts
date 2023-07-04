e764bee17321fae55f2327bcb9c25e74c769380d
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-07-04 13:42:08 +0630

    Refactor Entities.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,9 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient, { FetchResponse } from "../services/api-client";
 import useGameQueryStore from "../stores/gameQueryStore";
-import { Platform } from "./usePlatforms";
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-}
-
 const useGames = () => {
   const gameQuery = useGameQueryStore((s) => s.gameQuery);
   return useInfiniteQuery<FetchResponse<Game>, Error>({
